d6e40bb63ceab9b79b1c9c14484309ade6991e11
Juhi Joshi <juhiujoshi@gmail.com>
2021-01-07 23:27:06 -0500

    Initial Commit

---
+++ Painting List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Painting List
+//
+//  Created by New User1 on 1/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Painting List/Painting_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Painting_ListApp.swift
+//  Painting List
+//
+//  Created by New User1 on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Painting_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
